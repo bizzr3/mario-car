f2d1b3844ee39a2cc5bc79639ec2b0044ae9c801
bizzr3 <mojtaba.bakhtiaree@gmail.com>
2018-10-25 13:55:26 +0700

    Move function init commit

--- app/Http/Controllers/MarioController.php
+++ app/Http/Controllers/MarioController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use http\Exception;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
@@ -20,15 +18,101 @@ class MarioController extends Controller
 
     public function place(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'x' => 'required|string',
-            'y' => 'required|integer|min:1|max:5'
-        ]);
+        $validation = $this->validateCarLocation($request->all());
 
-        if ($validator->fails() || !in_array($request->get('x'), config('mario.nodes'))) {
+        if (!$validation) {
             return response()->json(false, Response::HTTP_BAD_REQUEST);
         }
 
         return response()->json(true, Response::HTTP_ACCEPTED);
+    }
+
+    public function move(Request $request)
+    {
+        $validation = $this->validateMove($request);
+
+        if (!$validation) {
+            return response()->json(false, Response::HTTP_BAD_REQUEST);
+        }
+
+        $y = $request->get('y');
+        $x = $request->get('x');
+        $nodes = config('mario.nodes');
+
+        //TODO: optimize this check and avoid accepting out of range indexes
+        try {
+            switch ($request->get('dir')) {
+                case 'u':
+                    {
+                        $y++;
+                        break;
+                    }
+                case 'd':
+                    {
+                        $y--;
+
+                        break;
+                    }
+                case 'l':
+                    {
+                        $x = $nodes[array_search($x, $nodes) - 1];
+
+                        break;
+                    }
+                case 'r':
+                    {
+                        $x = $nodes[array_search($x, $nodes) + 1];
+
+                        break;
+                    }
+            }
+        } catch (\Exception $e) {
+            return response()->json(false, Response::HTTP_BAD_REQUEST);
+        }
+
+        $validation = $this->validateCarLocation(['x' => $x, 'y' => $y]);
+
+        if (!$validation) {
+            return response()->json([
+                'can_move' => false
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        return response()->json([
+            'can_move' => true,
+            'x' => $x,
+            'y' => $y
+        ], Response::HTTP_ACCEPTED);
+    }
+
+    //TODO: merge validation methods.
+
+    private function validateCarLocation(array $param)
+    {
+        $validator = Validator::make($param, [
+            'x' => 'required|string',
+            'y' => 'required|integer|min:1|max:5'
+        ]);
+
+        if ($validator->fails() || !in_array($param['x'], config('mario.nodes'))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function validateMove(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'x' => 'required|string',
+            'y' => 'required|integer|min:1|max:5',
+            'dir' => 'required|string'
+        ]);
+
+        if ($validator->fails() || !in_array($request->get('x'), config('mario.nodes'))) {
+            return false;
+        }
+
+        return true;
     }
 }
